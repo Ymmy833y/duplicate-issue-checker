0656ce28d6c10f06dca32519791b6d3587bdb809
Ymmy <ymmy4989@gmail.com>
2024-11-15 19:41:45 +0900

    Form submission is done with fetch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
-     './app/templates/**/*.html'
+    './app/templates/**/*.html',
+    './app/static/**/*.js',
+  ],
+  safelist: [
+    {
+      pattern: /bg-(green|yellow|red|blue|purple|gray)-\d{2,3}/,
+    },
+    {
+      pattern: /text-(green|yellow|red|blue|purple|gray)-\d{2,3}/,
+    },
+    {
+      pattern: /border-(green|yellow|red|blue|purple|gray)-\d{2,3}/,
+    },
   ],
   theme: {
     extend: {},
